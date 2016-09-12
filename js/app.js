b08b63a8eebbf183813552bf21770bd2689a8d61
Maria Jose Tamayo y Ortiz <mariajose.tamayo@Marias-Mac-mini.local>
2016-09-11 20:58:23 -0600

    added overlay for instructions

--- js/app.js
+++ js/app.js
@@ -1,18 +1,28 @@
 $(document).ready(function(){
 
 
-function show(shown, hidden) {
-  document.getElementById(shown).style.display='block';
-  document.getElementById(hidden).style.display='none';
-  return false;
-}
+	function show(shown, hidden) {
+  		document.getElementById(shown).style.display='block';
+  		document.getElementById(hidden).style.display='none';
+  		return false;
+	}
 
-$("#submit").click(function(){
-	return show("page2", "page1")
-})
+	$("#submit").click(function(){
+		return show("page2", "page1")
+	})
 
-$("#searchAgain").click(function(){
-	return show("page1", "page2")
-})
+	$("#searchAgain").click(function(){
+		return show("page1", "page2")
+	})
 
-});
+	$(".what").click(function(){
+    		$(".overlay").fadeIn(1000);
+  	});
+
+ 	/*--- Hide information modal box ---*/
+  	
+	$("a.close").click(function(){
+  			$(".overlay").fadeOut(1000);
+ 	});
+
+	});
