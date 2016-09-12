3df56cb99bcc98a12f015f5df0b2249494e31a22
Maria Jose Tamayo y Ortiz <mariajose.tamayo@Marias-Mac-mini.local>
2016-09-12 17:57:34 -0600

    completed object for micronutrients

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+	//<--* function to change between page1 and page2 *-->
 
 	function show(shown, hidden) {
   		document.getElementById(shown).style.display='block';
@@ -7,7 +8,7 @@ $(document).ready(function(){
   		return false;
 	}
 
-	$("#submit").click(function(){
+	$("#submitBtn").click(function(){
 		return show("page2", "page1")
 	})
 
@@ -15,14 +16,81 @@ $(document).ready(function(){
 		return show("page1", "page2")
 	})
 
+	//<--* show instructions information modal box *-->
+
 	$(".what").click(function(){
     		$(".overlay").fadeIn(1000);
   	});
 
- 	/*--- Hide information modal box ---*/
+ 	 // <--* Hide instructions information modal box *-->
   	
 	$("a.close").click(function(){
   			$(".overlay").fadeOut(1000);
  	});
 
+	//<--* variables to obtain user input *-->
+
+	$('#submitBtn').click(function(event){
+  		event.preventDefault();
+  		var userFoodInput = document.getElementById("searchBox").value
+  		var diabetes = document.getElementById("diabetesBtn").value
+  		var kidney = document.getElementById("kidneyBtn").value
+  		var weight = document.getElementById("weightBtn").value
+  		var checkRecipe = document.getElementById('recipeCheck').checked;
+  		var recipePortion = document.getElementById("portionBox").value
+		console.log("siii", userFoodInput)
+		console.log("sii", recipePortion)
+  		getFoodRequest(userFoodInput)
 	});
+	
+
+	function getFoodRequest(searchTerm){
+
+  	//<--* En esta parte funcion llamamos y damos al servidor de youtube los parametros que solicita para mandarnos la informacion que necesitamos. *-->
+
+  		// var params = {
+    // 		"content-Type": "application/json",
+    // 		x-app-id: "c7faf842",
+    // 		x-app-key: "bc4a198b34d738f0f52d5f874775d99d"
+  		// };
+
+  //<--* Por medio de get, obtenemos obtenemos lo que necesitamos del servidor (en este caso videos correspondientes a la palabra que buscamos) y los ponemos dentro de la funcion showResults. Items es el key de cada video. *-->
+
+  		$.ajax({
+    		url: 'https://trackapi.nutritionix.com/v2/natural/nutrients',
+    		type: 'post',
+    		data: {
+ 			"query":"searchTerm",
+ 			"timezone": "US/Eastern"
+		},
+    		headers: {
+    			"Content-Type" : "application/json",
+        		"x-app-id": "c7faf842",   //If your header name has spaces or any other char not appropriate
+        		"x-app-key": "f06e1c4ce5415a0155b0c0010294b93e",  //for object property name, use quoted notation shown in second
+        		"x-remote-user-id" :0,
+    		},
+    		dataType: 'json',
+    		success: function (data) {
+        	console.info(data);
+    		}
+		});
+	}
+
+	// function showResults(results){
+
+ //  //<--* Aqui hacemos una variable de html vacia a la que luego le vamos a anadir los resultados de la busqueda (ie. los videos). Luego metemos esa variable al div contenido en el html, asi lo modificamos para que se muestren los resultados de la busqueda. *--> 
+
+ //  		var html = "";
+ //  		$.each(results, function(index,value){
+   
+ //   // <--* este codigo comentado es para que se muestren los videos en la pagina con thumbnails y al darle click lleve a una pagina separada de youtube. *-->
+
+ //      // html += '<li class="results">' +   value.snippet.title +  '<br/><br/>' + '<a href="https://www.youtube.com/watch?v=' + value.id.videoId +  '"><img class="thumbnail" src="' + value.snippet.thumbnails.medium.url+ '"></a>' + '<br/>' + '<p>' + value.snippet.description + '</p>' + '</li>' + '<br/><br/>'});
+
+ //  // <--* este es codigo moficado en el que pongo un id especifico a cada video para identificarlo unicamente y que cuando se le de click, se diferencie de los otros videos *-->
+
+ //      	html += '<li class="results">' +   value.snippet.title +  '<br/><br/>' + '<a><img id="'+ value.id.videoId+'" class="thumbnail" src="' + value.snippet.thumbnails.medium.url+ '"></a>' + '<br/>' + '<p>' + value.snippet.description + '</p>' + '</li>' + '<br/><br/>'
+
+ //    });
+
+});
---
+++ js/app2.js
@@ -0,0 +1,874 @@
+window.nutrientField = {
+  203 : {
+    usda_tag:"PROCNT",
+    name:"Protein",
+    unit:"g"
+  },
+  204 : {
+    usda_tag:"FAT",
+    name:"Total lipid (fat)",
+    unit:"g"
+  },
+  205 : {
+    usda_tag:"CHOCDF",
+    name:"Carbohydrate, by difference",
+    unit:"g"
+  },
+  207 : {
+    usda_tag:"ASH",
+    name:"Ash",
+    unit:"g"
+  }
+
+  208 : {
+    usda_tag:"ENERC_KCAL",
+    name:"Energy",
+    unit:"kcal"
+  }
+
+  209 : {
+    usda_tag:"STARCH",
+    name:"Starch",
+    unit:"g"
+  }
+
+  210 :{
+    usda_tag:"SUCS",
+    name:"Sucrose",
+    unit:"g"
+  }
+
+  211 : {
+    usda_tag:"GLUS",
+    name:"Glucose (dextrose)",
+    unit:"g"
+  }
+
+  212 : {
+    usda_tag:"FRUS",
+    name:"Fructose",
+    unit:"g"
+  }
+
+  213 : {
+    usda_tag:"LACS",
+    name:"Lactose",
+    unit:"g"
+  }
+
+  214 : {
+    usda_tag:"MALS",
+    name:"Maltose",
+    unit:"g"
+  }
+
+  221 : {
+    usda_tag:"ALC",
+    name:"Alcohol, ethyl",
+    unit:"g"
+  }
+
+  255 : {
+    usda_tag:"WATER",
+    name:"Water",
+    unit:"g"
+  }
+
+  257 : {
+    usda_tag:"NULL",
+    name:"Adjusted Protein",
+    unit:"g"
+  }
+
+  262 : {
+    usda_tag:"CAFFN",
+    name:"Caffeine",
+    unit:"mg"
+  }
+
+  263 :{
+    usda_tag:"THEBRN",
+    name:"Theobromine",
+    unit:"mg"
+  }
+
+  268 : {
+    usda_tag:"ENERC_KJ",
+    name:"Energy",
+    unit:"kJ"
+  }
+
+  269 : {
+    usda_tag:"SUGAR",
+    name:"Sugars, total",
+    unit:"g"
+  }
+
+  287 : {
+    usda_tag:"GALS",
+    name:"Galactose",
+    unit:"g"
+  }
+
+  291 : {
+    usda_tag:"FIBTG",
+    name:"Fiber, total dietary",
+    unit:"g"
+  }
+
+  301 : {
+    usda_tag:"CA",
+    name:"Calcium, Ca",
+    unit:"mg"
+  }
+
+  303 : { 
+    usda_tag:"FE",
+    name:"Iron, Fe",
+    unit:"mg"
+  }
+
+  304 : {
+    usda_tag:"MG",
+    name:"Magnesium, Mg",
+    unit:"mg"
+  }
+
+  305 : {
+    usda_tag:"P",
+    name:"Phosphorus, P",
+    unit:"mg"
+  }
+
+  306 : {
+    usda_tag:"K",
+    name:"Potassium, K",
+    unit:"mg"
+  }
+
+  307 : {
+    usda_tag:"NA",
+    name:"Sodium, Na",
+    unit:"mg"
+  }
+
+  309 : {
+    usda_tag:"ZN",
+    name:"Zinc, Zn",
+    unit:"mg"
+  }
+
+  312 : {
+    usda_tag:"CU",
+    name:"Copper, Cu",
+    unit:"mg"
+  }
+
+  313 : {
+    usda_tag:"FLD",
+    name:"Fluoride, F",
+    unit:"Âµg"
+  }
+
+  315 : {
+    usda_tag:"MN",
+    name:"Manganese, Mn",
+    unit:"mg"
+  }
+
+  317 : {
+    usda_tag:"SE",
+    name:"Selenium, Se",
+    unit:"Âµg"
+  }
+
+  318 : {
+    usda_tag:"VITA_IU",
+    name:"Vitamin A, IU",
+    unit:"IU"
+  }
+
+  319 : {
+    usda_tag:"RETOL",
+    name:"Retinol",
+    unit:"Âµg"
+  }
+
+  320 : {
+    usda_tag:"VITA_RAE",
+    name:"Vitamin A, RAE",
+    unit:"Âµg"
+  }
+
+  321 : {
+    usda_tag:"CARTB",
+    name:"Carotene, beta",
+    unit:"Âµg"
+  }
+
+  322 : {
+    usda_tag:"CARTA",
+    name:"Carotene, alpha",
+    unit:"Âµg"
+  }
+
+  323 : {
+    usda_tag:"TOCPHA",
+    name:"Vitamin E (alpha-tocopherol)",
+    unit:"mg"
+  }
+
+  324 : {
+    usda_tag:"VITD",
+    name:"Vitamin D",
+    unit:"IU"
+  }
+
+  325 : {
+    usda_tag:"ERGCAL",
+    name:"Vitamin D2 (ergocalciferol)",
+    unit:"Âµg"
+  }
+
+  326 : {
+    usda_tag:"CHOCAL",
+    name:"Vitamin D3 (cholecalciferol)",
+    unit:"Âµg"
+  }
+
+  328 : {
+    usda_tag:"VITD",
+    name:"Vitamin D (D2 + D3)",
+    unit:"Âµg"
+  }
+
+  334 : {
+    usda_tag:"CRYPX",
+    name:"Cryptoxanthin, beta",
+    unit:"Âµg"
+  }
+
+  337 : {
+    usda_tag:"LYCPN",
+    name:"Lycopene",
+    unit:"Âµg"
+  }
+
+  338 : {
+    usda_tag:"LUT+ZEA",
+    name:"Lutein + zeaxanthin",
+    unit:"Âµg"
+  }
+
+  341 : {
+    usda_tag:"TOCPHB",
+    name:"Tocopherol, beta",
+    unit:"mg"
+  }
+
+  342 : {
+    usda_tag:"TOCPHG",
+    name:"Tocopherol, gamma",
+    unit:"mg"
+  }
+
+  343 : {
+    usda_tag:"TOCPHD",
+    name:"Tocopherol, delta",
+    unit:"mg"
+  }
+
+  401 : {
+    usda_tag:"VITC",
+    name:"Vitamin C, total ascorbic acid",
+    unit:"mg"
+  }
+
+  404 : {
+    usda_tag:"THIA",
+    name:"Thiamin",
+    unit:"mg"
+  }
+
+  405 : {
+    usda_tag:"RIBF",
+    name:"Riboflavin",
+    unit:"mg"
+  }
+
+  406 : {
+    usda_tag:"NIA",
+    name:"Niacin",
+    unit:"mg"
+  }
+
+  410 : {
+    usda_tag:"PANTAC",
+    name:"Pantothenic acid",
+    unit:"mg"
+  }
+
+  415 : {
+    usda_tag:"VITB6A",
+    name:"Vitamin B-6",
+    unit:"mg"
+  }
+
+  417 : {
+    usda_tag:"FOL",
+    name:"Folate, total",
+    unit:"Âµg"
+  }
+
+  418 : {
+    usda_tag:"VITB12",
+    name:"Vitamin B-12",
+    unit:"Âµg"
+  }
+
+  421 : {
+    usda_tag:"CHOLN",
+    name:"Choline, total",
+    unit:"mg"
+  }
+
+  428 : {
+    usda_tag:"MK4",
+    name:"Menaquinone-4",
+    unit:"Âµg"
+  }
+
+  429 : {
+    usda_tag:"VITK1D",
+    name:"Dihydrophylloquinone",
+    unit:"Âµg"
+  }
+
+  430 : {
+    usda_tag:"VITK1",
+    name:"Vitamin K (phylloquinone)",
+    unit:"Âµg"
+  }
+
+  431 : {
+    usda_tag:"FOLAC",
+    name:"Folic acid",
+    unit:"Âµg"
+  }
+
+  432 : {
+    usda_tag:"FOLFD",
+    name:"Folate, food",
+    unit:"Âµg"
+  }
+
+  435 : {
+    usda_tag:"FOLDFE",
+    name:"Folate, DFE",
+    unit:"Âµg"
+  }
+
+  454 : {
+    usda_tag:"BETN",
+    name:"Betaine",
+    unit:"mg"
+  }
+
+  501 : {
+    usda_tag:"TRP_G",
+    name:"Tryptophan",
+    unit:"g"
+  }
+
+  502 : {
+    usda_tag:"THR_G",
+    name:"Threonine",
+    unit:"g"
+  }
+
+  503 : {
+    usda_tag:"ILE_G",
+    name:"Isoleucine",
+    unit:"g"
+  }
+
+  504 : {
+    usda_tag:"LEU_G",
+    name:"Leucine",
+    unit:"g"
+  }
+
+  505 : {
+    usda_tag:"LYS_G",
+    name:"Lysine",
+    unit:"g"
+  }
+
+  506 : {
+    usda_tag:"MET_G",
+    name:"Methionine",
+    unit:"g"
+  }
+
+  507 : {
+    usda_tag:"CYS_G",
+    name:"Cystine",
+    unit:"g"
+  }
+
+  508 : {
+    usda_tag:"PHE_G",
+    name:"Phenylalanine",
+    unit:"g"
+  }
+
+  509 : {
+    usda_tag:"TYR_G",
+    name:"Tyrosine",
+    unit:"g"
+  }
+
+  510 : {
+    usda_tag:"VAL_G",
+    name:"Valine",
+    unit:"g"
+  }
+
+  511 : {
+    usda_tag:"ARG_G",
+    name:"Arginine",
+    unit:"g"
+  }
+
+  512 : {
+    usda_tag:"HISTN_G",
+    name:"Histidine",
+    unit:"g"
+  }
+
+  513 : {
+    usda_tag:"ALA_G",
+    name:"Alanine",
+    unit:"g"
+  }
+
+  514 : {
+    usda_tag:"ASP_G",
+    name:"Aspartic acid",
+    unit:"g"
+  }
+
+  515 : {
+    usda_tag:"GLU_G",
+    name:"Glutamic acid",
+    unit:"g"
+  }
+
+  516 : {
+    usda_tag:"GLY_G",
+    name:"Glycine",
+    unit:"g"
+  }
+
+  517 : {
+    usda_tag:"PRO_G",
+    name:"Proline",
+    unit:"g"
+  }
+
+  518 : {
+    usda_tag:"SER_G",
+    name:"Serine",
+    unit:"g"
+  }
+
+  521 : {
+    usda_tag:"HYP",
+    name:"Hydroxyproline",
+    unit:"g"
+  }
+
+  573 : {
+    usda_tag:"NULL",
+    name:"Vitamin E, added",
+    unit:"mg"
+  }
+
+  578 : {
+    usda_tag:"NULL",
+    name:"Vitamin B-12, added",
+    unit:"Âµg"
+  }
+
+  601 : {
+    usda_tag:"CHOLE",
+    name:"Cholesterol",
+    unit:"mg"
+  }
+
+  605 : {
+    usda_tag:"FATRN",
+    name:"Fatty acids, total trans",
+    unit:"g"
+  }
+
+  606 : {
+    usda_tag:"FASAT",
+    name:"Fatty acids, total saturated",
+    unit:"g"
+  }
+
+  607 : {
+    usda_tag:"F4D0",
+    name:"4:00",
+    unit:"g"
+  }
+
+  608 : {
+    usda_tag:"F6D0",
+    name:"6:00",
+    unit:"g"
+  }
+
+  609 : {
+    usda_tag:"F8D0",
+    name:"8:00",
+    unit:"g"
+  }
+
+  610 :{
+    usda_tag:"F10D0",
+    name:"10:00",
+    unit:"g"
+  } 
+
+  611 : {
+    usda_tag:"F12D0",
+    name:"12:00",
+    unit:"g"
+  }
+
+  612 : {
+    usda_tag:"F14D0",
+    name:"14:00",
+    unit:"g"
+  }
+
+  613 : {
+    usda_tag:"F16D0",
+    name:"16:00",
+    unit:"g"
+  }
+
+  614 : {
+    usda_tag:"F18D0",
+    name:"18:00",
+    unit:"g"
+  }
+
+  615 : {
+    usda_tag:"F20D0",
+    name:"20:00",
+    unit:"g"
+  }
+
+  617 : {
+    usda_tag:"F18D1",
+    name:"18:1 undifferentiated",
+    unit:"g"
+  }
+
+  618 : {
+    usda_tag:"F18D2",
+    name:"18:2 undifferentiated",
+    unit:"g"
+  }
+
+  619  : {
+    usda_tag:"F18D3",
+    name:"18:3 undifferentiated",
+    unit:"g"
+  }
+
+  620 : {
+    usda_tag:"F20D4",
+    name:"20:4 undifferentiated",
+    unit:"g"
+  }
+
+  621 : {
+    usda_tag:"F22D6",
+    name:"22:6 n-3 (DHA)",
+    unit:"g"
+  }
+
+  624 : {
+    usda_tag:"F22D0",
+    name:"22:00",
+    unit:"g"
+  }
+
+  625 : {
+    usda_tag:"F14D1",
+    name:"14:01",
+    unit:"g"
+  }
+
+  626 : {
+    usda_tag:"F16D1",
+    name:"16:1 undifferentiated",
+    unit:"g"
+  }
+
+  627 : {
+    usda_tag:"F18D4",
+    name:"18:04",
+    unit:"g"
+  }
+
+  628 : {
+    usda_tag:"F20D1",
+    name:"20:01",
+    unit:"g"
+  }
+
+  629 : {
+    usda_tag:"F20D5",
+    name:"20:5 n-3 (EPA)",
+    unit:"g"
+  }
+
+  630 : {
+    usda_tag:"F22D1",
+    name:"22:1 undifferentiated",
+    unit:"g"
+  }
+
+  631 : {
+    usda_tag:"F22D5",
+    name:"22:5 n-3 (DPA)",
+    unit:"g"
+  }
+
+  636 : {
+    usda_tag:"PHYSTR",
+    name:"Phytosterols",
+    unit:"mg"
+  }
+
+  638 : {
+    usda_tag:"STID7",
+    name:"Stigmasterol",
+    unit:"mg"
+  }
+
+  639 : {
+    usda_tag:"CAMD5",
+    name:"Campesterol",
+    unit:"mg"
+  }
+
+  641 : {
+    usda_tag:"SITSTR",
+    name:"Beta-sitosterol",
+    unit:"mg"
+  }
+
+  645 : {
+    usda_tag:"FAMS",
+    name:"Fatty acids, total monounsaturated",
+    unit:"g"
+  }
+  
+  646 : {
+    usda_tag:"FAPU",
+    name:"Fatty acids, total polyunsaturated",
+    unit:"g"
+  }
+
+  652 : {
+    usda_tag:"F15D0",
+    name:"15:00",
+    unit:"g"
+  }
+
+  653 : {
+    usda_tag:"F17D0",
+    name:"17:00",
+    unit:"g"
+  }
+
+  654 : {
+    usda_tag:"F24D0",
+    name:"24:00:00",
+    unit:"g"
+  }
+
+  662 : {
+    usda_tag:"F16D1T",
+    name:"16:1 t",
+    unit:"g"
+  }
+
+  663 : {
+    usda_tag:"F18D1T",
+    name:"18:1 t",
+    unit:"g"
+  }
+
+  664 : {
+    usda_tag:"NULL",
+    name:"22:1 t",
+    unit:"g"
+  }
+
+  665 : {
+    usda_tag:"NULL",
+    name:"18:2 t not further defined",
+    unit:"g"
+  }
+
+  666 : {
+    usda_tag:"NULL",
+    name:"18:2 i",
+    unit:"g"
+  }
+
+  669 : {
+    usda_tag:"F18D2TT",
+    name:"18:2 t,t",
+    unit:"g"
+  }
+
+  670 : {
+    usda_tag:"F18D2CLA",
+    name:"18:2 CLAs",
+    unit:"g"
+  }
+
+  671 : {
+    usda_tag:"F24D1C",
+    name:"24:1 c",
+    unit:"g"
+  }
+
+  672 : {
+    usda_tag:"F20D2CN6",
+    name:"20:2 n-6 c,c",
+    unit:"g"
+  }
+
+  673 : {
+    usda_tag:"F16D1C",
+    name:"16:1 c",
+    unit:"g"
+  }
+
+  674 : {
+    usda_tag:"F18D1C",
+    name:"18:1 c",
+    unit:"g"
+  }
+
+  675 : {
+    usda_tag:"F18D2CN6",
+    name:"18:2 n-6 c,c",
+    unit:"g"
+  }
+
+  676 : {
+    usda_tag:"NULL",
+    name:"22:1 c",
+    unit:"g"
+  }
+
+  685 : {
+    usda_tag:"F18D3CN6",
+    name:"18:3 n-6 c,c,c",
+    unit:"g"
+  }
+
+  687 : {
+    usda_tag:"F17D1",
+    name:"17:01",
+    unit:"g"
+  }
+
+  689 : {
+    usda_tag:"F20D3",
+    name:"20:3 undifferentiated",
+    unit:"g"
+  }
+
+  693 : {
+    usda_tag:"FATRNM",
+    name:"Fatty acids, total trans-monoenoic",
+    unit:"g"
+  }
+
+  695 : {
+    usda_tag:"FATRNP",
+    name:"Fatty acids, total trans-polyenoic",
+    unit:"g"
+  }
+
+  696 : {
+    usda_tag:"F13D0",
+    name:"13:00",
+    unit:"g"
+  }
+
+  697 : {
+    usda_tag:"F15D1",
+    name:"15:01",
+    unit:"g"
+  }
+
+  851 : {
+    usda_tag:"F18D3CN3",
+    name:"18:3 n-3 c,c,c (ALA)",
+    unit:"g"
+  }
+
+  852 : {
+    usda_tag:"F20D3N3",
+    name:"20:3 n-3",
+    unit:"g"
+  }
+
+  853 : {
+    usda_tag:"F20D3N6",
+    name:"20:3 n-6",
+    unit:"g"
+  }
+
+  855 : {
+    usda_tag:"F20D4N6",
+    name:"20:4 n-6",
+    unit:"g"
+  }
+
+  856 : {
+    usda_tag:"NULL",
+    name:"18:3i",
+    unit:"g"
+  }
+
+  857 : {
+    usda_tag:"F21D5",
+    name:"21:05",
+    unit:"g"
+  }
+
+  858 : {
+    usda_tag:"F22D4",
+    name:"22:04",
+    unit:"g"
+  }
+
+  859 : {
+    usda_tag:"F18D1TN7",
+    name:"18:1-11t (18:1t n-7)",
+    unit:"g"
+  }
+}
