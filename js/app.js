18288f3fc2a636fe3073adf57182b2b5fba6168c
Maria Jose Tamayo y Ortiz <mariajose.tamayo@Marias-Mac-mini.local>
2016-09-12 20:33:36 -0600

    trying to get data from API but failing

--- js/app.js
+++ js/app.js
@@ -46,51 +46,52 @@ $(document).ready(function(){
 
 	function getFoodRequest(searchTerm){
 
-  	//<--* En esta parte funcion llamamos y damos al servidor de youtube los parametros que solicita para mandarnos la informacion que necesitamos. *-->
+  // 		$.ajax({
+  // 			async: true,
+  // 			crossDomain: true,
+  //   		url: 'https://trackapi.nutritionix.com/v2/natural/nutrients',
+  //   		processData: false,
+  //   		method: 'POST',
+  //   		data: {
+	 // 			query: "apple",
+	 // 			timezone: "US/Eastern",
+		// 	},
+  //   		headers: {
+  //   			"content-type": "application/json",
+		//     	"x-app-id": "c7faf842",
+		//     	"x-app-key": "bc4a198b34d738f0f52d5f874775d99d",
+  //   		},
+		// }).done(function (response) {
+		// 	console.log(response)
+		// });
 
-  		// var params = {
-    // 		"content-Type": "application/json",
-    // 		x-app-id: "c7faf842",
-    // 		x-app-key: "bc4a198b34d738f0f52d5f874775d99d"
-  		// };
+		// var data = JSON.stringify({
+		//   	"query": searchTerm,
+		//   	"timezone": "US/Eastern"
+		// })
 
-  //<--* Por medio de get, obtenemos obtenemos lo que necesitamos del servidor (en este caso videos correspondientes a la palabra que buscamos) y los ponemos dentro de la funcion showResults. Items es el key de cada video. *-->
+		var settings = {
+		  	url: "https://trackapi.nutritionix.com/v2/natural/nutrients",
+		  	method: "POST",
+		  	headers: {
+		    	"content-type": "application/json",
+		    	"x-app-id": "c7faf842",
+		    	"x-app-key": "bc4a198b34d738f0f52d5f874775d99d",
+		  	},
+		  	data: "{\n\t\"query\":\"bigmac\",\n\t\"timezone\":\"US/Eastern\"\n}\n\n"
+		 //  	data: {
+	 	// 		"query": "apple",
+	 	// 		"timezone": "US/Eastern",
+			// },
+			// error: function (err) {
+			// 	console.log(err)
+			// }
+		}
 
-  		$.ajax({
-    		url: 'https://trackapi.nutritionix.com/v2/natural/nutrients',
-    		type: 'post',
-    		data: {
- 			"query":"searchTerm",
- 			"timezone": "US/Eastern"
-		},
-    		headers: {
-    			"Content-Type" : "application/json",
-        		"x-app-id": "c7faf842",   //If your header name has spaces or any other char not appropriate
-        		"x-app-key": "f06e1c4ce5415a0155b0c0010294b93e",  //for object property name, use quoted notation shown in second
-        		"x-remote-user-id" :0,
-    		},
-    		dataType: 'json',
-    		success: function (data) {
-        	console.info(data);
-    		}
+		$.ajax(settings).done(function (response) {
+		  	console.log(response);
 		});
 	}
 
-	// function showResults(results){
-
- //  //<--* Aqui hacemos una variable de html vacia a la que luego le vamos a anadir los resultados de la busqueda (ie. los videos). Luego metemos esa variable al div contenido en el html, asi lo modificamos para que se muestren los resultados de la busqueda. *--> 
-
- //  		var html = "";
- //  		$.each(results, function(index,value){
-   
- //   // <--* este codigo comentado es para que se muestren los videos en la pagina con thumbnails y al darle click lleve a una pagina separada de youtube. *-->
-
- //      // html += '<li class="results">' +   value.snippet.title +  '<br/><br/>' + '<a href="https://www.youtube.com/watch?v=' + value.id.videoId +  '"><img class="thumbnail" src="' + value.snippet.thumbnails.medium.url+ '"></a>' + '<br/>' + '<p>' + value.snippet.description + '</p>' + '</li>' + '<br/><br/>'});
-
- //  // <--* este es codigo moficado en el que pongo un id especifico a cada video para identificarlo unicamente y que cuando se le de click, se diferencie de los otros videos *-->
-
- //      	html += '<li class="results">' +   value.snippet.title +  '<br/><br/>' + '<a><img id="'+ value.id.videoId+'" class="thumbnail" src="' + value.snippet.thumbnails.medium.url+ '"></a>' + '<br/>' + '<p>' + value.snippet.description + '</p>' + '</li>' + '<br/><br/>'
-
- //    });
-
+	
 });
