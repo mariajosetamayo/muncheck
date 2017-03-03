f964224da0532a64e01b04bebaea3e15b5aceff8
Maria Jose Tamayo <mariajose.tamayo@gmail.com>
2017-03-03 10:04:20 -0700

    refactoring to JS

--- js/app.js
+++ js/app.js
@@ -1,169 +1,106 @@
-$(document).ready(function(){
+
+
+	////// Variables to obtain elements from the DOM //////
+
+	// View 1 //
+
+	var searchFoodBox = $("#searchBox");
+	var diseasesBtns = $(".disease");
+	var submitFoodQueryBtn = $("#submitBtn");
+	var showInstructionsBtn = $(".what");
+	var instructionsOverlay = $(".overlay");
+	var closeOverlayBtn = $("#close");
+
+	// View 2 //
+
+	var resultsContainer = $(".resultsContainer");
+	var recommendationContainer = $("#results2");
+	var foodPhoto = $("#foodPhoto");
+	var foodItemNameContainer = $("#foodItem");
+	var micronutrientsResultsContainer = $("#micronutrientsResults");
+	var nutritionalInfoTableHeading = $(".performance-facts__title");
+	var caloriesTableField = $("#calories");
+	var totalFatTableField = $("#total_fat");
+	var saturatedFatTableField = $("#saturated_fat");
+	var cholesterolTableField = $("#cholesterol");
+	var sodiumTableField = $("#sodium");
+	var totalCarbsTableField = $("#total_carbs");
+	var dietaryFiberTableField = $("#dietary_fiber");
+	var sugarTableField = $("#sugar");
+	var proteinTableField = $("#protein");
+	var searchFoodAgainBtn = $(".searchAgain");
+
+	// View 1 and 2 //
+
+	var muncheckLogo = $(".logo");
+
+	////// Variable to access the micronutrient information from the nutrientField object in nutrientFieldObj.js /////
+
+	var allNutrientInfo = window.nutrientField;
+
+	////// State Object for keeping track of User Preferences //////
 
 	var userPrefs = {
 		hasDiabetes: false,
 		hasKidneyDisease: false,
 		wantsToLoseWeight: false,
 	};
-	var allNutrientInfo = window.nutrientField;
 
-	//<--* function to change between page1 and page2 *-->
+	////// Application's logic ///////
 
-	function show(shown, hidden) {
-  		document.getElementById(shown).style.display='block';
-  		document.getElementById(hidden).style.display='none';
-  		return false;
-	};
-
-	$("#submitBtn").click(function(){
-		return show("page2", "page1");
-	});
-
-	$(".searchAgain").click(function(){
-		userPrefs.hasDiabetes = false;
-		userPrefs.hasKidneyDisease = false;
-		userPrefs.wantsToLoseWeight = false;
-		$("#searchBox").val("");
-		userPrefs[window.selectedDisease] =  false;
-		$(".disease").removeClass("diseaseToggle");
-		$(".resultsContainer").removeClass("notFoundContainer");
-		return show("page1", "page2");
-	});
-
-	//<--* show instructions information modal box *-->
-
-	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
-  	});
-
- 	 // <--* Hide instructions information modal box *-->
-  	
-	$("#close").click(function(){
-  		$(".overlay").fadeOut(1000);
- 	});
-
- 	$(".logo").click(function(){
- 		$("#searchBox").val("");
- 	});
-
-
-
-	//<--* variables to obtain user input *-->
-
-	$(".disease").click(function(event){
-		event.preventDefault();
-		window.selectedDisease = $(this).attr("id");// obtienes el id con jquery.
-		console.log("yeii", selectedDisease);
-		userPrefs[window.selectedDisease] = true;
-		console.log("noo", userPrefs);
-		$(this).toggleClass("diseaseToggle");// jQuery Para que se vea aplastado (pintarle verde).
-	});
-	
-	$("#submitBtn").click(function(event){
-  		event.preventDefault();
-  		var userFoodInput = document.getElementById("searchBox").value;
-  		getFoodRequest(userFoodInput);
-  		
-	});
-
-	
+	// AJAX call to the Nutritionix API //
 
 	function getFoodRequest(searchTerm){
 
 		var searchTerm= searchTerm;
 		var data = {
- 			"query": searchTerm,
-  			"timezone": "US/Eastern",
+			"query": searchTerm,
+			"timezone": "US/Eastern",
 		};
 
 		$.ajax({
-  			url: 'https://trackapi.nutritionix.com/v2/natural/nutrients',
-  			type: 'POST',
-  			data: JSON.stringify(data),
-  			headers: {
-    			"Content-Type" : "application/json",
-    			"x-app-id": "c7faf842",  
-    			"x-app-key": "bc4a198b34d738f0f52d5f874775d99d",  
-  			},
-  			dataType: 'json',
-  			success: function (data) {
-    			// Este console.log te muestra el objeto que retornas
-    			console.info(data);
-    
-    			//Mostrar elemento en pantalla
-    			showNutritionalValue(data);
-    			var isRecommended = isFoodRecommended(data);
+			url: 'https://trackapi.nutritionix.com/v2/natural/nutrients',
+			type: 'POST',
+			data: JSON.stringify(data),
+			headers: {
+				"Content-Type" : "application/json",
+				"x-app-id": "c7faf842",
+				"x-app-key": "bc4a198b34d738f0f52d5f874775d99d",
+			},
+			dataType: 'json',
+			success: function (data) {
+				// Optional console log to see the data that is returned from Nutritionix API
+				// console.info(data);
+				showNutritionalValue(data);
+				var isRecommended = isFoodRecommended(data);
 				showRecomendation(isRecommended);
-  			},
-  			error: function (request, status, error) {
-    			// Estos console.log te dan informacion del error.
-    			console.log(request.responseText);
-    			console.log(error);
-    			console.log(status);
-    			var errorElem = showError(error);
-  			},
+			},
+			error: function (request, status, error) {
+				// Optional console logs to get information about the errors returned from the Nutritionix API
+				// console.log(request.responseText);
+				// console.log(error);
+				// console.log(status);
+				var errorElem = showError(error);
+			},
 		});
 	}
 
-	var showError = function(error){
-		$(".resultsContainer").addClass("notFoundContainer");
-		var errorText = $("#results2").append("<h2> This food is not available. Please search again. </h2>");
-		errorElem.append(errorText);
-	};
-	
-  	function showNutritionalValue(data){
-  // Funcion que contiene el jQuery para mostrar en la pantalla.
-		// Esto es la separacion de concerns. En la de arriba se hace el query, en
-		// esta se muestra todo en pantalla.
-		document.getElementById("foodItem").value = "";
-		document.getElementById("foodItem").value = "";
-		$("#foodPhoto").attr('src', data.foods[0].photo.thumb);
-		$("#foodItem").text(data.foods[0].food_name);
-
-		for (var i=0; i< data.foods[0].full_nutrients.length; i++){
-			var nutrient = data.foods[0].full_nutrients[i];
-			var nutrientID = nutrient.attr_id;
-			// console.log(nutrientID); te imprime toodos los ides de los full_nutrients, un opor uno.
-			if (window.nutrientField[nutrientID]) {
-				var name = window.nutrientField[nutrientID].name;
-				if (nutrient.value !== 0) {
-					$("#micronutrientsResults").append(
-						"<li>" + name + ": " + nutrient.value + " " + window.nutrientField[nutrientID].unit + "</li>"
-					);
-				}
-			}
-		}
-
-		$(".performance-facts__title").text("Nutrition Information       "+ data.foods[0].serving_weight_grams + " grams");
-		$("#calories").text(data.foods[0].nf_calories + " Kcal");
-		$("#total_fat").text(data.foods[0].nf_total_fat + " g");
-		$("#saturated_fat").text(data.foods[0].nf_saturated_fat + " g");
-		$("#cholesterol").text(data.foods[0].nf_cholesterol + "mg");
-		$("#sodium").text(data.foods[0].nf_sodium + " mg");
-		$("#total_carbs").text(data.foods[0].nf_total_carbohydrate + " g");
-		$("#dietary_fiber").text(data.foods[0].nf_dietary_fiber + " g");
-		$("#sugar").text(data.foods[0].nf_sugars + " g");
-		$("#protein").text(data.foods[0].nf_protein + " g");
-	
-	}
+	// Function for getting a recommendation for the food query //
 
 	function isFoodRecommended(data){
 		var isRecommended = true;
 		for (var i=0; i< data.foods[0].full_nutrients.length; i++){
 			var nutrient = data.foods[0].full_nutrients[i];
 			var nutrientID = nutrient.attr_id;
-			// console.log(nutrientID); te imprime toodos los ides de los full_nutrients, un opor uno.
 			if(nutrientID === 305){
-			 	var phosphorus = nutrient.value;
-				console.log(phosphorus);
+				var phosphorus = nutrient.value;
 			}
 			if(nutrientID === 605){
-			 	var transFat = nutrient.value;
-				console.log(phosphorus);
+				var transFat = nutrient.value;
 			}
 		}
 
-		if (userPrefs.hasDiabetes){ // solo se ejecuta si el usuario selecciono ese boton en la primera pantalla.
+		if (userPrefs.hasDiabetes){
 			if((data.foods[0].nf_total_carbohydrate>=30) || (data.foods[0].nf_saturated_fat>1) || (transFat>0) || (data.foods[0].serving_weight_grams<15 && data.foods[0].nf_calories> 30) || (data.foods[0].serving_weight_grams<15 && data.foods[0].nf_sugars> 3)){
 				isRecommended = false;
 			}
@@ -187,17 +124,111 @@ $(document).ready(function(){
 		return isRecommended;
 	};
 
+	/////// Event listeners for View 1 ////////
+
+	submitFoodQueryBtn.click(function(){
+		return showCorrectView("page2", "page1");
+	});
+
+	showInstructionsBtn.click(function(){
+		instructionsOverlay.fadeIn(1000);
+	});
+
+	closeOverlayBtn.click(function(){
+		instructionsOverlay.fadeOut(1000);
+	});
+
+	muncheckLogo.click(function(){
+		searchFoodBox.val("");
+	});
+
+	diseasesBtns.click(function(event){
+		event.preventDefault();
+		window.selectedDisease = $(this).attr("id");
+		userPrefs[window.selectedDisease] = true;
+		$(this).toggleClass("diseaseToggle"); // Changes background color of buttons when clicked.
+	});
+
+	submitFoodQueryBtn.click(function(event){
+		event.preventDefault();
+		var userFoodInput = document.getElementById("searchBox").value;
+		getFoodRequest(userFoodInput);
+	});
+
+	/////// Event listeners for View 2 ///////
+
+	searchFoodAgainBtn.click(function(){
+		userPrefs.hasDiabetes = false;
+		userPrefs.hasKidneyDisease = false;
+		userPrefs.wantsToLoseWeight = false;
+		searchFoodBox.val("");
+		userPrefs[window.selectedDisease] =  false;
+		diseasesBtns.removeClass("diseaseToggle");
+		resultsContainer .removeClass("notFoundContainer");
+		return showCorrectView("page1", "page2");
+	});
+
+	/////// DOM manipulation for View 1 and 2 ///////
+
+	function showCorrectView(shown, hidden) {
+		document.getElementById(shown).style.display='block';
+		document.getElementById(hidden).style.display='none';
+		return false;
+	};
+
+	/////// DOM manipulation for View 2 ///////
+
+	var showError = function(error){
+		resultsContainer .addClass("notFoundContainer");
+		var errorText = recommendationContainer.append("<h2> This food is not available. Please search again. </h2>");
+		errorElem.append(errorText);
+	};
+
+	function showNutritionalValue(data){
+		document.getElementById("foodItem").value = "";
+		document.getElementById("foodItem").value = "";
+		foodPhoto.attr('src', data.foods[0].photo.thumb);
+		foodItemNameContainer.text(data.foods[0].food_name);
+
+		for (var i=0; i< data.foods[0].full_nutrients.length; i++){
+			var nutrient = data.foods[0].full_nutrients[i];
+			var nutrientID = nutrient.attr_id;
+			if (window.nutrientField[nutrientID]) {
+				var name = window.nutrientField[nutrientID].name;
+				if (nutrient.value !== 0) {
+					micronutrientsResultsContainer.append(
+						"<li>" + name + ": " + nutrient.value + " " + window.nutrientField[nutrientID].unit + "</li>"
+					);
+				}
+			}
+		}
+
+		nutritionalInfoTableHeading.text("Nutrition information for a portion size of:"+ " " + " " + data.foods[0].serving_weight_grams + " grams");
+		caloriesTableField.text(data.foods[0].nf_calories + " Kcal");
+		totalFatTableField .text(data.foods[0].nf_total_fat + " g");
+		saturatedFatTableField.text(data.foods[0].nf_saturated_fat + " g");
+		cholesterolTableField.text(data.foods[0].nf_cholesterol + "mg");
+		sodiumTableField.text(data.foods[0].nf_sodium + " mg");
+		totalCarbsTableField.text(data.foods[0].nf_total_carbohydrate + " g");
+		dietaryFiberTableField.text(data.foods[0].nf_dietary_fiber + " g");
+		sugarTableField.text(data.foods[0].nf_sugars + " g");
+		proteinTableField.text(data.foods[0].nf_protein + " g");
+
+	}
+
 	function showRecomendation(isRecommended){
-		$("#results2").empty()
+		recommendationContainer.empty()
 		if (isRecommended){
-			$(".resultsContainer").addClass("recommendedContainer");//Pintar de verde el div
-			$(".resultsContainer").removeClass("notRecommendedContainer");
-			$("#results2").append("<h2> Recommended!!! </h2>");
+			resultsContainer.addClass("recommendedContainer"); //changes background to green
+			resultsContainer.removeClass("notRecommendedContainer");
+			recommendationContainer.append("<h2> Recommended!!! </h2>");
 		} else  {
-			// Aparezca texto "Not Rec"
-			$("#results2").append("<h2> Not Recommended!!! </h2>");
-			$(".resultsContainer").addClass("notRecommendedContainer");//Pintarle de rojo al texto.
-			$(".resultsContainer").removeClass("recommendedContainer");
+			recommendationContainer.append("<h2> Not Recommended!!! </h2>");
+			resultsContainer.addClass("notRecommendedContainer"); //changes background to red
+			resultsContainer.removeClass("recommendedContainer");
 		}
 	};
+
+$(document).ready(function(){
+	showCorrectView();
 });
